6c7b718a2f735270884e6729dbc14e4202ad3c01
sandsq <sandsquaretinkerbrain@gmail.com>
2022-10-19 13:42:14 -0400

    revert to regular tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,18 +4,18 @@ using Test
 
 
 
-open("data/2701-0.txt", "r") do fi
-    ucount = 0
-    ecount = 0
-    tot = 0
-    s = read(fi, String)
-    for c in s
-        if isuppercase(c) ucount += 1 end
-        if c == 'c' ecount += 1 end
-        tot += 1
-    end
-    println("uppercount $ucount ecount $ecount total $tot")
-end
+# open("data/2701-0.txt", "r") do fi
+#     ucount = 0
+#     ecount = 0
+#     tot = 0
+#     s = read(fi, String)
+#     for c in s
+#         if isuppercase(c) ucount += 1 end
+#         if c == 'c' ecount += 1 end
+#         tot += 1
+#     end
+#     println("uppercount $ucount ecount $ecount total $tot")
+# end
 
-# include("test_textprocessor.jl")
+include("test_textprocessor.jl")
 
